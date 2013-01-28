40fdf06cd2b1aafc626874ef89ae0e56b58081f2
Liming Hong <hong_liming@163.com>
2013-01-29 01:16:08 +0800

    refine usteel

--- scraper/scraper/spiders/usteel.py
+++ scraper/scraper/spiders/usteel.py
@@ -9,22 +9,23 @@ class UsteelSpider(CrawlSpider):
     start_urls = ["http://shop.usteel.com/index.php?app=gangcai"]
 
     rules = (
-        Rule(SgmlLinkExtractor(allow=('index.php', )), callback = 'parse_item'),
+        Rule(SgmlLinkExtractor(allow=('index.php\?app=gangcai', )), callback = 'parse_item'),
+        Rule(SgmlLinkExtractor(allow=('index.php\?app=buxiu', )), callback = 'parse_item'),
     )
 
     def parse_item(self, response):
         hxs = HtmlXPathSelector(response)
-        webItems = hxs.select('//*//div[@class="result mb10"]//ul[@class="list"]')
+        webItems = hxs.select('//*//div[@class="result mb10"]//ul[@class="list"]/li')
         objects = [];
         for webItem in webItems:
             object = SteelItem()
-            object['name']              = webItem.select('//span[@class="s1 textL"]/a/@title').extract()
-            object['model']             = webItem.select('//span[@class="s2"]/text()').extract()
-            object['size']              = webItem.select('//span[@class="s3"]/text()').extract()
-            object['producer']          = webItem.select('//span[@class="s4"]/text()').extract()
-            object['producer_location'] = webItem.select('//span[@class="s5"]/text()').extract()
-            object['price']             = webItem.select('//span[@class="s6"]/b/text()').extract()
-            object['stock']             = webItem.select('//span[@class="s6"]/i/text()').extract()
-            object['reseller']          = webItem.select('//span[@class="s8 textL"]/a/@title').extract()
+            object['name']              = webItem.select('*/span[@class="s1 textL"]/a/@title').extract()[0]
+            object['model']             = webItem.select('*/span[@class="s2"]/text()').extract()[0]
+            object['size']              = webItem.select('*/span[@class="s3"]/text()').extract()[0]
+            object['producer']          = webItem.select('*/span[@class="s4"]/text()').extract()[0]
+            object['producer_location'] = webItem.select('*/span[@class="s5"]/text()').extract()[0]
+            object['price']             = webItem.select('*/span[@class="s6"]/b/text()').extract()[0]
+            object['stock']             = webItem.select('*/span[@class="s6"]/i/text()').extract()[0]
+            object['reseller']          = webItem.select('*/span[@class="s8 textL"]/a/@title').extract()[0]
             objects.append(object)
         return objects
